d51f0c453be3f4b6320201c24b9aa704fff73740
JuanLeal1105 <JUAN.LCRUZ@mail.escuelaing.edu.co>
2026-01-27 22:44:44 -0500

    feat:Se hace un main para la primera parte un poco más funcional

--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -6,20 +6,25 @@
 package edu.eci.arsw.threads;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author hcadavid
  */
 public class CountThreadsMain {
     public static void main(String a[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese start o run dependiendo del método que desee usar: ");
+        String choice = sc.nextLine().toLowerCase();
+
+
         List<CountThread> threads = new ArrayList<>();
 
         threads.add(new CountThread(0, 99));
         threads.add(new CountThread(99, 199));
         threads.add(new CountThread(200, 299));
 
-        for (CountThread t : threads) {
-            t.run();
-        }
+        threads.forEach(choice.equals("start") ? CountThread::start : CountThread::run);
     }
 }
